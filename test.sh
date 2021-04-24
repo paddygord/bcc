3aff32a1a4db1093eb1db229a4142a1cb9e2b310
Patrick Gordon <pgpatrickgordon@gmail.com>
2021-04-24 22:52:32 +0100

    split clean

--- test.sh
+++ test.sh
@@ -2,10 +2,8 @@
 
 set -euxo pipefail
 
-rm -rf out packaged
 if [[ ! -d out ]]; then
     export CC=clang
-    export PKG_CONFIG_PATH=out
     meson setup out
 fi
 meson install -C out --only-changed
